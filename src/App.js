b86728f45b47dfc5ce4b7f19a3a746bfc944e8ed
Gustavo Rangel <gustavosrg@outlook.com>
2021-12-24 21:02:24 -0300

    Add repository included

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories]= useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo Repositório`,
+      url: 'https://github.com/Novo_repositorio',
+      techs: ['Node.js', 'Reactjs']
+
+    })
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +32,15 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+          {repository.title}
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
